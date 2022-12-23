7a197332665587e1086b7311a9091ad6c87eeb84
aviathota <aviathota@gmail.com>
2022-12-23 16:07:10 -0500

    125 python solution

--- Python/125ValidPalindrome.py
+++ Python/125ValidPalindrome.py
@@ -1,3 +1,7 @@
+# We first convert the string into a lowercase version of itself using lower(). Then, we take the alphanumeric characters of s using a for loop and isalnum().
+# We use join() instead of creating a new string and appending to it because it's faster.
+# The last line checks if the string is the same forwards and backwards (i.e. is it a palindrome).
+
 class Solution(object):
     def isPalindrome(self, s):
         """
